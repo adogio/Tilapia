d506e0d8b6ae47e20637e44d2095e4b629c3aa82
WMXPY <wm@mengw.io>
2017-10-16 02:04:37 -0500

    tilapiaify binary tree

--- src/tree/binaryTree.ts
+++ src/tree/binaryTree.ts
@@ -1,4 +1,5 @@
 import node from './node';
+import tilapia from '../interface';
 
 class binaryTree<T> {
 
@@ -6,6 +7,30 @@ class binaryTree<T> {
 
     public constructor(value: T) {
         this.root = new node(value);
+    }
+
+    public getRoot(): node<T> {
+        return this.root;
+    }
+
+    public addNode(): binaryTree<T> {
+        throw new Error("Tilapia need a add module to do this, try npm install tilapia-binary-tree-balance");
+    }
+
+    public search(value: T): T {
+        throw new Error("Tilapia need a search module to do this, try npm install tilapia-binary-tree-balance");
+    }
+
+    public tilapia(uses: Array<tilapia>): binaryTree<T> {
+        for (let i: number = 0; i < uses.length; i++) {
+            this.use(uses[i]);
+        }
+        return this;
+    }
+
+    public use(use: tilapia): binaryTree<T> {
+        this[use.target] = use.func.bind(this);
+        return this;
     }
 }
 
--- src/tree/node.ts
+++ src/tree/node.ts
@@ -2,12 +2,50 @@
 class node<T>{
 
     private value: T;
-    private leftChild: node<T>;
-    private rightChild: node<T>;
+    private _leftChild: node<T>;
+    private _rightChild: node<T>;
 
     public constructor(value: T) {
         this.value = value;
     }
+
+    public set left(value: T) {
+        this._leftChild = new node<T>(value);
+    }
+
+    public get left(): T {
+        return this._leftChild.getValue();
+    }
+
+    public set right(value: T) {
+        this._rightChild = new node<T>(value);
+    }
+
+    public get right(): T {
+        return this._rightChild.getValue();
+    }
+
+    public leftChild(): node<T> {
+        return this._leftChild;
+    }
+
+    public rightChild(): node<T> {
+        return this._rightChild;
+    }
+
+    public isLeft(): boolean {
+        return Boolean(this._leftChild);
+    }
+
+    public isRight(): boolean {
+        return Boolean(this._rightChild);
+    }
+
+    public getValue(): T {
+        return this.value;
+    }
+
+    public
 
 }
 
