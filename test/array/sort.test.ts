170ae28bfab0ef76a082e6146d6c3300ff40125e
WMXPY <wm@mengw.io>
2017-11-03 11:11:29 -0500

    testing

--- test/array/sort.test.ts
+++ test/array/sort.test.ts
@@ -10,10 +10,10 @@ describe('Sort function', () => {
     })
 
     it('should return same when useing sorted array', () => {
-        expect([1, 2, 3]["devideSort"]()).to.deep.equal([1, 2, 3]);
+        expect([1, 2, 3]["devideSort"]()).to.be.deep.equal([1, 2, 3]);
     });
 
     it('should return sorted version when useing unsort array', () => {
-        expect([3, 6, 2, 1, 7, 4]["devideSort"]()).to.deep.equal([1, 2, 3, 4, 6, 7]);
+        expect([3, 6, 2, 1, 7, 4]["devideSort"]()).to.be.deep.equal([1, 2, 3, 4, 6, 7]);
     });
 });
